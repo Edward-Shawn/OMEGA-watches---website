f422e9f97ebbdff3bb9cd1b21832b1e1019a2bfb
Edward-Shawn <sbh3834@naver.com>
2020-08-08 23:48:28 +0900

    하단페이지 완성 및 상단 메인베너 로고 및 텍스트 삽

--- js/custom.js
+++ js/custom.js
@@ -1,14 +1,14 @@
-$(document).ready(function(){
-    var bannerBtn = $(".banner_btn").height();
-    var winWidth = $(window).height();
+// $(document).ready(function(){
+//     var bannerBtn = $(".banner_btn").height();
+//     var winWidth = $(window).height();
 
-    $(window).resize(function(){
-        if($(window).width()>1300){
-            $(".banner_wrap").height(winWidth-bannerBtn);
-        } else {
-            $(".banner_wrap").css("height", "auto");
-        }
-    });
+//     $(window).resize(function(){
+//         if($(window).width()>1300){
+//             $(".banner_wrap").height(winWidth-bannerBtn);
+//         } else {
+//             $(".banner_wrap").css("height", "auto");
+//         }
+//     });
 
 
-});
+// });
