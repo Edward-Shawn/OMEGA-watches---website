fd3ec3ac2f69e60e58a4007059405535c8d08b91
Edward-Shawn <sbh3834@naver.com>
2020-08-10 23:59:26 +0900

    no message

--- js/custom.js
+++ js/custom.js
@@ -1,15 +1,21 @@
 $(document).ready(function(){
+    
 
     $(".menu").click(function(){
         $(".menu_wrap").fadeIn("fast")
         $(".main_menu").css('left','0');
-        $("body").css('overflow-y','hidden')
+        // $("body").css('overflow-y','hidden')
+        $(".menu_wrap").on('scroll mousewheel', function(e){
+            e.preventDefault();
+            e.stopPropagation();
+            return false;
+        })
 
     })
 
     $(".main_menu button").click(function(){
         $(".menu_wrap").css('display','none');
         $(".main_menu").css('left','-230px');
-        $("body").css('overflow-y','unset');
+        // $("body").css('overflow-y','unset');
     })
 });
